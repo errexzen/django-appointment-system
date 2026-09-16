8d1ed1d2903e0bafbac060208204b610d2962730
amirghorbanpour <amirrghorbanpour@gmail.com>
2026-09-16 23:36:39 +0330

    phase 5.02
    email auth

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,54 @@
-from django.contrib.auth.models import AbstractUser
+import secrets
+
+from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
+
+
+class CustomUserManager(UserManager):
+	def _create_user(self, email, password, **extra_fields):
+		if not email:
+			raise ValueError("Email must be provided")
+		email = self.normalize_email(email)
+		username = (extra_fields.get("username") or email.split("@")[0]).strip() or "user"
+		if self.model._default_manager.filter(username=username).exists():
+			username = f"{username}-{secrets.token_hex(3)}"
+		extra_fields["username"] = username
+		user = self.model(email=email, **extra_fields)
+		user.set_password(password)
+		user.save(using=self._db)
+		return user
+
+	def create_user(self, username=None, email=None, password=None, **extra_fields):
+		if email is None and username and "@" in str(username):
+			email = username
+			username = None
+		elif email is None:
+			email = username or extra_fields.pop("email", None)
+			username = None
+		if not email:
+			raise ValueError("Email must be provided")
+		extra_fields.setdefault("username", username or email.split("@")[0])
+		extra_fields.setdefault("is_staff", False)
+		extra_fields.setdefault("is_superuser", False)
+		return self._create_user(email, password, **extra_fields)
+
+	def create_superuser(self, username=None, email=None, password=None, **extra_fields):
+		if email is None and username and "@" in str(username):
+			email = username
+			username = None
+		elif email is None:
+			email = username or extra_fields.pop("email", None)
+			username = None
+		if not email:
+			raise ValueError("Email must be provided")
+		extra_fields.setdefault("username", username or email.split("@")[0])
+		extra_fields.setdefault("is_staff", True)
+		extra_fields.setdefault("is_superuser", True)
+		if extra_fields.get("is_staff") is not True:
+			raise ValueError("Superuser must have is_staff=True.")
+		if extra_fields.get("is_superuser") is not True:
+			raise ValueError("Superuser must have is_superuser=True.")
+		return self._create_user(email, password, **extra_fields)
 
 
 class UserRole(models.TextChoices):
@@ -10,6 +59,7 @@ class UserRole(models.TextChoices):
 
 
 class User(AbstractUser):
+	email = models.EmailField(unique=True)
 	phone_number = models.CharField(max_length=20, blank=True)
 	role = models.CharField(
 		max_length=10,
@@ -17,5 +67,10 @@ class User(AbstractUser):
 		default=UserRole.CUSTOMER,
 	)
 
+	USERNAME_FIELD = "email"
+	REQUIRED_FIELDS = []
+
+	objects = CustomUserManager()
+
 	def __str__(self):
-		return self.username
+		return self.email
--- accounts/serializers.py
+++ accounts/serializers.py
@@ -8,25 +8,45 @@ User = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, min_length=8)
+    accept_terms = serializers.BooleanField(write_only=True, required=False, default=True)
+    accept_privacy = serializers.BooleanField(write_only=True, required=False, default=True)
 
     class Meta:
         model = User
-        fields = ("id", "username", "email", "phone_number", "password")
+        fields = (
+            "id",
+            "email",
+            "phone_number",
+            "first_name",
+            "last_name",
+            "password",
+            "accept_terms",
+            "accept_privacy",
+        )
 
     def create(self, validated_data):
+        accept_terms = validated_data.pop("accept_terms", True)
+        accept_privacy = validated_data.pop("accept_privacy", True)
+        if (accept_terms is False) or (accept_privacy is False):
+            raise serializers.ValidationError("Terms and privacy policy acceptance are required")
         password = validated_data.pop("password")
-        user = User(**validated_data)
-        user.set_password(password)
-        user.save()
+        user = User.objects.create_user(password=password, **validated_data)
         return user
 
 
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    username_field = User.EMAIL_FIELD
+
     @classmethod
     def get_token(cls, user):
         token = super().get_token(user)
-        token["username"] = user.username
+        token["email"] = user.email
         return token
+
+    def validate(self, attrs):
+        if "email" in attrs and "password" in attrs:
+            attrs[self.username_field] = attrs.pop("email")
+        return super().validate(attrs)
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
